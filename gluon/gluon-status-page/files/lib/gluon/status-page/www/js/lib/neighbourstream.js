cd04b6b71969fbd23494b7a76ed4eb79dbf9916d
Nils Schneider <nils@nilsschneider.net>
2014-08-20 23:09:11 +0200

    refactor NeighbourStream

--- gluon/gluon-status-page/files/lib/gluon/status-page/www/js/lib/neighbourstream.js
+++ gluon/gluon-status-page/files/lib/gluon/status-page/www/js/lib/neighbourstream.js
@@ -3,178 +3,133 @@ define([ "vendor/bacon"
        , "lib/streams"
        ], function(Bacon, Helper, Streams) {
 
-  function mkNeighbourStream(ip) {
-    var out = new Bacon.Bus();
-    Helper.request(ip, "interfaces").then(function (d) {
-      magic(ip, d, out);
-    });
+  return function (mgmtBus, ip) {
+    return Bacon.fromBinder(function (sink) {
+      function magic(interfaces) {
+        var bus = new Bacon.Bus();
+        var querier = nodeQuerier(bus);
+        querier.map(".nodeInfo").onValue(mgmtBus.pushEvent("nodeinfo"));
 
-    return out;
-  }
+        var macsToNodeId = querier.scan({}, foldMacs);
 
-  function magic(ip, interfaces, output) {
-    var bus = new Bacon.Bus();
+        var stations = [];
+        for (var ifname in interfaces) {
+          var stream = new Streams.stations(ip, ifname);
+          stream = stream.map(function (d) {
+            for (var station in d)
+              d[station].ifname = ifname;
 
-    var querier = nodeQuerier(ip, bus);
+            return d;
+          });
 
-    querier = querier.scan({ nodes: {}
-                           , macs: {}
-                           }, foldQuerier);
+          stations.push(stream.toProperty({}));
+        }
 
-    var stations = []
-    for (var ifname in interfaces) {
-      var stream = new Streams.stations(ip, ifname);
-      stream = stream.map(
-        function (d) {
-          for (var station in d)
-            d[station].ifname = ifname;
+        var wifiStream = Bacon.combineAsArray(stations).map(function (d) {
+          return d.reduce(function (p, c) {
+            for (var station in c)
+              p[station] = c[station];
 
-          return d;
+            return p;
+          }, {});
         });
-      stations.push(stream.toProperty({}));
-    }
 
-    var wifiStream = Bacon.combineAsArray(stations).map(function (d) {
-      return d.reduce(function (p, c) {
-        for (var station in c)
-          p[station] = c[station];
+        var batadvStream = new Streams.batadv(ip);
 
-        return p;
-      }, {});
-    });
+        var stream3 = wifiStream.combine(batadvStream.toProperty({}), combineWifiBatadv)
 
-    var batadvStream = new Streams.batadv(ip);
+        stream3 = Bacon.combineTemplate({ stations: stream3
+                                        , macs: macsToNodeId
+                                        })
+                        .scan({ asked: {}, neighbours: {}}, foldNeighbours(bus))
+                        .map(".neighbours");
 
-    var stream3 = wifiStream.combine(batadvStream.toProperty({}), combineWifiBatadv)
+        stream3.onValue(sink);
+      }
 
-    stream3 = Bacon.combineTemplate({ stations: stream3
-                                    , nodes: querier.toProperty()
-                                    })
-                    .scan({ internal: { asked: []
-                                      }
-                          , external: { neighbours: {}
-                                      , nodes: {}
-                                      }
-                          }, foldNeighbours(bus))
-                    .map(".external");
+      function nodeQuerier(bus) {
+        var out = new Bacon.Bus();
 
+        bus.onValue(function (ifname) {
+          var stream = Streams.nodeInfo(ip, ifname);
+          out.plug(stream.map(function (d) {
+            return { "ifname": ifname
+                   , "nodeInfo": d
+                   };
+          }));
+        });
 
-    output.plug(stream3);
-  }
+        return out;
+      }
 
+      function foldMacs(a, node) {
+        node.nodeInfo.network.mesh_interfaces.forEach(function (mac) {
+          a[mac] = node.nodeInfo.node_id;
+        });
 
-  function nodeQuerier(ip, bus) {
-    var out = new Bacon.Bus();
-  
-    bus.onValue(function (ifname) {
-      var stream = Streams.nodeInfo(ip, ifname);
-      out.plug(stream.scan({}, addNode));
-    });
+        return a;
+      }
 
+      function foldNeighbours(bus) {
+        return function(a, b) {
+          var now = new Date().getTime();
 
-    return out;
-  }
+          var toAsk = new Bacon.Bus();
+          bus.plug(toAsk.skipDuplicates());
 
-  function addNode(a, b) {
-    if (!(b.node_id in a))
-      a[b.node_id] = b;
+          for (var mac in a.asked)
+            if (now - a.asked[mac] > 5 * 1000)
+              delete a.asked[mac];
 
-    return a;
-  }
+          for (var station in b.stations)
+            if (station in b.macs) {
+              b.stations[station].nodeId = b.macs[station];
+            } else if (!(station in a.asked)) {
+              a.asked[station] = now;
+              toAsk.push(b.stations[station].ifname);
+            }
 
-  function foldQuerier(a, nodes) {
-    for (var nodeId in nodes) {
-      if (!(nodeId in a.nodes))
-        a.nodes[nodeId] = nodes[nodeId];
+          toAsk.end();
 
-      var macs = nodes[nodeId].network.mesh_interfaces;
+          a.neighbours = b.stations;
 
-      macs.forEach(function (mac) {
-        if (!(mac in a.macs))
-          a.macs[mac] = nodeId;
-      });
-    }
-
-    return a;
-  }
-
-  function arrayNub(a) {
-    return a.sort().reduce(
-      function (a, b) {
-        if (a.length == 0)
-          return [b];
-        else if (b != a[a.length-1])
-          return a.push(b);
-        else
           return a;
-      }, []);
-  }
-
-  function haveAsked(a, s) {
-    return a.filter(function (d) {
-      return d.station == s;
-    }).length > 0;
-  }
-
-  function foldNeighbours(bus) {
-    return function(a, b) {
-      var toAsk = [];
-
-      var now = new Date().getTime();
-
-      a.internal.asked = a.internal.asked.filter(function (d) {
-        return (now - d.timestamp) < 5 * 1000;
-      });
-
-      for (var station in b.stations) {
-        if ("node" in b.stations[station]) {
-          continue;
-        } else if (station in b.nodes.macs) {
-          b.stations[station].nodeId = b.nodes.macs[station];
-        } else if (!haveAsked(a.internal.asked, station)) {
-          a.internal.asked.push({ station: station
-                                , timestamp: new Date().getTime()
-                                });
-          toAsk.push(b.stations[station].ifname);
         }
       }
 
-      arrayNub(toAsk).forEach(bus.push);
+      function batadvToDict(batadv) {
+        var o = {};
 
-      a.external.neighbours = b.stations;
-      a.external.nodes = b.nodes.nodes;
+        for (var station in batadv) {
+          var a = batadv[station];
 
-      return a;
-    }
-  }
+          if (!(a.ifname in o))
+            o[a.ifname] = {};
 
-  function batadvToDict(batadv) {
-    var o = {};
+          o[a.ifname][station] = {};
+          o[a.ifname][station].batadvTQ = a.tq;
+        }
 
-    for (var station in batadv) {
-      var a = batadv[station];
-
-      if (!(a.ifname in o))
-        o[a.ifname] = {};
-
-      o[a.ifname][station] = {};
-      o[a.ifname][station].batadvTQ = a.tq;
-    }
-
-    return o;
-  }
-
-  function combineWifiBatadv(wifi, batadv) {
-    for (var station in batadv) {
-      if (!(station in wifi))
-        wifi[station] = {}
-
-      for (var a in batadv[station])
-        wifi[station][a] = batadv[station][a];
+        return o;
       }
 
-    return wifi;
-  }
+      function combineWifiBatadv(wifi, batadv) {
+        for (var station in batadv) {
+          if (!(station in wifi))
+            wifi[station] = {}
 
-  return mkNeighbourStream;
+          for (var a in batadv[station])
+            wifi[station][a] = batadv[station][a];
+          }
+
+        return wifi;
+      }
+
+      Helper.request(ip, "interfaces").then(magic);
+
+      return function () {
+        console.log("FIXME, unsubscribe");
+      }
+    });
+  }
 })
